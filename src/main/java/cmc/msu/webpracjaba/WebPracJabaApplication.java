ac589c898b743a303934aa0c17c3694ac1e99148
matthew <matvey.zvancov10@gmail.com>
2024-02-25 16:17:43 +0300

    Initial commit

---
+++ src/main/java/cmc/msu/webpracjaba/WebPracJabaApplication.java
@@ -0,0 +1,13 @@
+package cmc.msu.webpracjaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPracJabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPracJabaApplication.class, args);
+    }
+
+}
